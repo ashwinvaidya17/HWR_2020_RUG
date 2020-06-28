baff02c6e5554fe3248f69b13f0a49e2003e879c
k-nonymous <24710772+krishkribo@users.noreply.github.com>
2020-06-28 19:45:48 +0200

    Update main.py

--- main.py
+++ main.py
@@ -4,23 +4,22 @@ import shutil
 import argparse
 
 
-
-
 if __name__=="__main__":
     # ----------------------------- line segmentation -----------------------------------------------
     arg=argparse.ArgumentParser()
     arg.add_argument('--image',metavar='I',type=str,help="enter the image path")
     img_parase=arg.parse_args()
-    img=img_parase.image
+    img_path=img_parase.image
     print(f"------------------------------>> Line segmentation <<------------------------------------")
     curr_dir=os.getcwd()
     if not "lines" in os.listdir():
         os.mkdir("lines")
-    split1=img.split('.')
-    split2=split1[0].split('/')
-    line_segment.run(img,"lines/"+split2[-1],curr_dir)
+    for img in os.listdir(img_path):
+        split1=img.split('.')
+        line_segment.run(img_path+"/"+img,"lines/"+split1[0],curr_dir)
 
     print("# Line segmentation completed #")
 
     # -------------------------- character segmentation -------------------------------------------
     print(f"------------------------------>> character segmentation <<------------------------------------")
+
