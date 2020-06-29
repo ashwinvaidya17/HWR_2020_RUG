ace60c2bce9a34b1a6dbecd676059d07d51eb497
Ashwin Vaidya <ashwinnitinvaidya@gmail.com>
2020-06-29 21:34:08 +0200

    Add comments and code cleanup

--- character_segmentor/ConnectedComponents.py
+++ character_segmentor/ConnectedComponents.py
@@ -9,19 +9,22 @@ import time
 
 
 def segment_lines(docs_path, image_name, output_path, lines):
+    # Store the save path in a variable
     save_loc = output_path + '/' + lines + \
         "/{:04d}/".format(int(image_name.split('.')[0]))
     os.makedirs(save_loc, exist_ok=True)
+    # Read line image
     original_image = cv2.imread(os.path.join(
         docs_path, image_name), cv2.IMREAD_GRAYSCALE)
-    line_image = original_image.copy()  # cv2.blur(original_image, (15, 15))
+    line_image = original_image.copy()
     boxes_locations = []
 
-    # plt.show()
+    # Binarizing the image helps increase the segmentation accuracy
     window_size = 3
     thresh_sauvola = threshold_sauvola(line_image, window_size=window_size)
     binary_sauvola = line_image > thresh_sauvola
     binary_sauvola = np.uint8(binary_sauvola)
+    # Find the contours
     ctrs, hier = cv2.findContours(
         binary_sauvola.copy(), cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
     sorted_ctrs = sorted(ctrs, key=lambda ctr: cv2.boundingRect(ctr)[0])
@@ -30,12 +33,13 @@ def segment_lines(docs_path, image_name, output_path, lines):
         x, y, w, h = cv2.boundingRect(ctr)
 
         roi = line_image[y:y + h, x:x + w]
-        # plt.imshow(roi, 'gray')
-        # plt.show()
+
         area = w*h
+        # Check if the identified character is valid based on area size
         if area > 900 and area < 100000:
             boxes_locations.append([x, y, x + w, y + h])
 
+    # save all identified characters
     for i, box in tqdm.tqdm(enumerate(boxes_locations)):
         x1, y1, x2, y2 = box
         character_img = original_image[y1:y2, x1:x2].copy()
@@ -44,13 +48,15 @@ def segment_lines(docs_path, image_name, output_path, lines):
 
 
 def iterate_over_folders(docs_path, output_images_path, img_name):
+    """
+    This is the entry point for this module.
+    args:
+    docs_path: path of the folder which contains seperated lines
+    output_images_path: the output directry to save the segmented files in
+    img_name: name of the document
+    """
     start = time.process_time()
+    # Go through each line in the document folder
     for image_name in tqdm.tqdm(os.listdir(docs_path)):
         segment_lines(docs_path, image_name, output_images_path, img_name)
     print("Time taken", time.process_time() - start)
-
-
-if __name__ == "__main__":
-    docs_path = "C:/Users/ashwi/Desktop/Handwriting Recognition/Codes/output_images/line_images"
-    output_images_path = 'C:/Users/ashwi/Desktop/Handwriting Recognition/Codes/output_images/character_images'
-    # iterate_over_folders(docs_path, output_images_path)
--- character_segmentor/template_matching.py
+++ character_segmentor/template_matching.py
@@ -29,7 +29,7 @@ def get_match_scores(search_image, threshold=0.52):
     for label in categories:
         match_scores[label] = []
     for label in categories:
-        # Take first n images
+        # Do template matching on all the images of all the categories.
         for path in glob.glob(character_image_path+label+'/*.pgm'):
             template = cv2.imread(path, 0)
             template_x, template_y = template.shape
@@ -37,7 +37,7 @@ def get_match_scores(search_image, threshold=0.52):
             if scale_ratio > 2:  # Experiments found this scale better
                 scale_ratio = 2
             search_image_x, search_image_y = search_image.shape
-            # for scale in np.arange(0.2, scale_ratio, 0.1):
+
             y_ratio = (int)(scale_ratio*template_x)
             if y_ratio > search_image_x:
                 y_ratio = search_image_x
@@ -50,8 +50,7 @@ def get_match_scores(search_image, threshold=0.52):
                 template, (x_ratio, y_ratio), interpolation=cv2.INTER_AREA)
 
             w, h = template.shape[::-1]
-            # print(search_image.shape)
-            # print(template.shape)
+            # Incase the resize dimentions are not valid. Happens if the split image has one of the dimension as 0
             try:
                 res = cv2.matchTemplate(search_image, template, method)
             except Exception as e:
@@ -74,6 +73,13 @@ def get_match_scores(search_image, threshold=0.52):
 
 
 def split_images(input_image, top_left, h, w):
+    """Split the image into three regions.
+    first_image is assumed to be before the extracted region.
+    extracted_image is the center image
+    second_image is the third image
+    returns:
+    three tuples of extracted image and its area ratio
+    """
     extracted_image = input_image.copy(
     )[:top_left[1]+h, top_left[0]: top_left[0]+w]
     first_image = input_image.copy()[:top_left[1]+h, :top_left[0]]
@@ -94,33 +100,35 @@ def split_images(input_image, top_left, h, w):
 def recursive_splitting(img, recursion_depth, area_ratio, image_index, search_threshold=0.6):
     global unique_counter
     #   Base cases
-    if recursion_depth > 5:  # Safety
+    if recursion_depth > 5:  # Safety to avoid infinite loop. 5 is more than enough
         return
 
+    # in case one of the recursion returns None. Can happen in case of save or some other condition
     if img is None:
         return
 
+    # Compute and store the save path once
     unique_save_path = output_save_path + '/' + \
         "{:04d}".format(image_index) + '_' + \
         "{:04d}".format(unique_counter)+'.jpg'
-    # print(unique_save_path)
-    # print(orig_x, orig_y)
+
+    # if the match covers this much area then stop recursion and just save the image
     if area_ratio > 0.7:  # Based on experiments
-        # img = cv2.resize(img, (32, 49))
+
         plt.imsave(unique_save_path,
                    img, cmap='gray')
         unique_counter += 1
         return
 
-    # print("Getting match scores")
     match_scores = get_match_scores(img, search_threshold)
-#     print(match_scores)
-    # print("Found %s matches" % len(match_scores))
+
+    # return from recursion if the image has no matches
     if len(match_scores.values()) == 0:
         return
 
+    # Save image if only one class is found
     if len(match_scores) == 1:
-        # img = cv2.resize(img, (32, 49))
+
         plt.imsave(unique_save_path,
                    img, cmap='gray')
         unique_counter += 1
@@ -130,15 +138,9 @@ def recursive_splitting(img, recursion_depth, area_ratio, image_index, search_th
 
     best_match_details = {}
     temp_image = img.copy()
-    # Take first n images
+    # Go through all the images of the class to do template matching and find the best match
     for path in glob.glob(character_image_path+max_label+'/*.pgm'):
         template = cv2.imread(path, 0)
-        # plt.imshow(template)
-        # print(template.shape)
-        # plt.show()
-        # print(orig_x, orig_y)
-        # plt.imshow(temp_image)
-        # plt.show()
         template_x, template_y = template.shape
         scale_ratio = orig_x/template_x
         if scale_ratio > 2:  # Experiments found this scale better
@@ -165,9 +167,7 @@ def recursive_splitting(img, recursion_depth, area_ratio, image_index, search_th
             print(e)
             return
         min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(res)
-        # plt.imshow(template)
-        # print(template.shape)
-        # plt.show()
+
         w, h = template.shape[::-1]
         best_match_details[max_val] = max_loc
     max_key = max(best_match_details.keys())
@@ -175,7 +175,8 @@ def recursive_splitting(img, recursion_depth, area_ratio, image_index, search_th
     (extracted_image, extracted_image_ar), (first_image, first_image_ar), (second_image,
                                                                            second_image_ar) = split_images(temp_image, top_left, h, w)
 
-    #  Area ratio found based on experiments
+    #  Area ratio found based on experiments. Area ratio is found based on the original image.
+    # If it ends up splitting upto this amount then assume that this region does not contain any character
     if extracted_image_ar < 0.2:
         extracted_image = None
     if first_image_ar < 0.2:
@@ -193,6 +194,14 @@ def recursive_splitting(img, recursion_depth, area_ratio, image_index, search_th
 
 
 def iterate_over_characters(input_path, character_images_path):
+    """
+    This is the entry point for this module.
+    args:
+    input_path: path of the character images
+    character_images_path: path to store the output
+    """
+    # some variables are declared global so that they can be shared without unnecessary parameters in the functions.
+    # This needs to be called whenever a global variable is assigned in a function so that it does not make a local copy
     global categories
     global unique_counter
     global orig_x
@@ -202,36 +211,24 @@ def iterate_over_characters(input_path, character_images_path):
     character_image_path = character_images_path
     categories = os.listdir(character_image_path)
     start = time.process_time()
+    # Iterate over each line
     for line_character_folders in os.listdir(os.path.join(input_path)):
         print("Computing for line", line_character_folders)
+        # Iterate over each character in the image
         for input_image_path in tqdm.tqdm(glob.glob(os.path.join(input_path,  line_character_folders)+"/*.jpg")):
             input_character_image = cv2.imread(input_image_path, 0)
             orig_x, orig_y = input_character_image.shape
-            # Based on experiments. If the ratio is greater then it is assumed that the segemented image is a combination
+            # Based on experiments. If the ratio is greater then it is assumed that the segemented image is a combination of few characters
             if (orig_y/orig_x) < 1.2:
                 continue
             unique_counter = 1
             output_save_path = os.path.join(
                 input_path, line_character_folders)
-            # todo change path for linux
             image_index = int(input_image_path.split('.')
-                              [0].split('\\')[-1])
+                              [0].split('/')[-1])
             os.makedirs(output_save_path, exist_ok=True)
+            # Split the character
             recursive_splitting(input_character_image, 1, 0, image_index)
+            # Remove original character image as it has been split
             os.remove(input_image_path)
     print("Time taken", time.process_time() - start)
-
-
-if __name__ == "__main__":
-    # parser = argparse.ArgumentParser(
-    #     description='This file splits each character into sub-characters as the first character splitting might be coarse.')
-    # parser.add_argument(
-    #     '--input_path', help='Path of the coarse split characters')
-    # parser.add_argument('--character_images',
-    #                     help='Path to folder containing character images for use in template matching')
-    # args = parser.parse_args()
-
-    input_path = "C:/Users/ashwi/Desktop/Handwriting Recognition/Codes/output_images/character_images/"  # args.input_path
-    # args.character_images
-    character_image_path = "C:/Users/ashwi/Desktop/Handwriting Recognition/Images/"
-    iterate_over_characters(input_path, character_image_path)
